027d1239cd3f16be0f42ef23ac3eb0fdc6f9f928
Juliette Chevalier <juliettech@Juliettes-MacBook-Pro-3.local>
2020-05-13 16:56:35 -0500

    updated footer to add link to twitter account

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import '../Steps/Html/index.scss';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import './index.scss';
+
+const admiosLogo = require('../../images/admios-logo.png');
 
 export default function Footer(): JSX.Element {
   return (
     <footer>
-      <p>This page was build with lots of 💙 by Juliette Chevalier.</p>
+        <a href="https://twitter.com/_juliettech">
+        <p>This page was build with lots of 💙 by
+          Juliette Chevalier.
+        </p>
+      </a>
+
+      <img src={admiosLogo} alt="Admios" width="200px" />
     </footer>
   )
 }
